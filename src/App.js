3ad552bfdcf1c94ed9d8c165752082f35fd3eb20
Dustin McKinney <55887574+IamDusty@users.noreply.github.com>
2025-04-10 23:18:24 -0400

    href to new pages

--- src/App.js
+++ src/App.js
@@ -323,7 +323,7 @@ function App() {
                     <ul className="py-2">
                       <li>
                         <a
-                          href="./caltracker/index.html"
+                          href="https://iamdusty.github.io/CalTracker/"
                           className="block px-4 py-2 text-sm text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700"
                         >
                           Calorie Tracker
